fe7fca28bff7844ef50cc939757759258419dccf
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2023-01-20 15:57:21 +0100

    first project setup

---
+++ src/Questions.js
@@ -0,0 +1,7 @@
+import React, { useState } from 'react'
+import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai'
+const Questions = () => {
+  return <h2>question component</h2>
+}
+
+export default Questions
