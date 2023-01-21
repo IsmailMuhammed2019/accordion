94d4851d0bb5bf5f7ae26b5571abf15e6a0ee332
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2023-01-21 17:09:40 +0100

    add show info functionalities

--- src/Questions.js
+++ src/Questions.js
@@ -1,7 +1,20 @@
 import React, { useState } from 'react'
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai'
-const Questions = () => {
-  return <h2>question component</h2>
+const Questions = (question) => {
+
+    const [show, setShow] = useState(false)
+    const {id, title, info} = question
+  return (
+    <article className='question'>
+      <header>
+        <h4>{title}</h4>
+        <button className='btn' onClick={() => setShow(!show)}>
+          {show ? <AiOutlineMinus /> :  <AiOutlinePlus />}
+        </button>
+      </header>
+      {show && <p>{info}</p>}
+    </article>
+  )
 }
 
 export default Questions
