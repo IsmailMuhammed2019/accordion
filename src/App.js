2844dd39d96c53735c0c266383bf64ad5a62181b
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2023-01-21 16:56:40 +0100

    setup questions useState and component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,23 @@ import React, { useState } from 'react'
 import data from './data'
 import SingleQuestion from './Questions'
 function App() {
-  return <h2>accordion project setup</h2>
+          const [questions, setQuestions] = useState(data)
+  return (
+
+    <main>
+      <div className="container">
+        <h4>Questions And Answers About Login</h4>
+        <div className="info">
+          {questions.map((question) => {
+            return (
+              <SingleQuestion key={question.id} {...question} />
+            )
+          })}
+        </div>
+      </div>
+
+    </main>
+  )
 }
 
 export default App
